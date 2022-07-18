cdcb560bc751aa2d501ad4bf203155cd401b5250
alpysaruzhan <alpysaruzan4@gmail.com>
2022-07-18 17:50:57 +0600

    comm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang7';
+  public d = false;
+  public f = false;
+  public a = false;
+  public g = false;
+  public t = false;
+
+  fir(){
+    setTimeout(() => {
+      this.d = true
+    }, 50)
+  }
+  fir1(){
+    setTimeout(() => {
+      this.f = true
+    }, 50)
+  }
+  fir2(){
+    setTimeout(() => {
+      this.a = true
+    }, 50)
+  }
+  fir3(){
+    setTimeout(() => {
+      this.g = true
+    }, 50)
+  }
+  firs(){
+    setTimeout(() => {
+      this.t = true
+    }, 50)
+  }
 }
